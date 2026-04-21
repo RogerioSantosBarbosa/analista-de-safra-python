01c8f84f286a9d49599448d142bf750420eac87e
RogerioSantosBarbosa <DevSemCash@gmail.com>
2026-04-21 18:58:22 -0300

    feat: transforma rota da API em dinâmica e adiciona tratamento de erro 404

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import pandas as pd
 from flask import Flask, jsonify
 
-# Inicializando o servidor
 app = Flask(__name__)
 
-# Criando a "porta de atendimento" (Rota da API)
-@app.route('/api/laudo', methods=['GET'])
-def gerar_laudo():
+# 1. Rota Dinâmica: O Flask agora captura a palavra que vem depois da barra
+@app.route('/api/laudo/<nome_cultura>', methods=['GET'])
+def gerar_laudo(nome_cultura):
     tabela = pd.read_csv("dados_safra.csv")
-    MEDIA_SAFRA_REFERENCIA = (150.00)
+    MEDIA_SAFRA_REFERENCIA = 150.00
 
-    # 1. FILTRO E CÁLCULO
-    tabela_filtrada = tabela[tabela['cultura'] == 'Milho']
+    # 2. O filtro agora usa a variável que veio da URL
+    tabela_filtrada = tabela[tabela['cultura'] == nome_cultura]
+
+    # TRAVA DE SEGURANÇA: Caso o usuário digitar uma cultura que não existe no CSV
+    if tabela_filtrada.empty:
+        # Devolvemos um JSON de erro e o código 404 (Não Encontrado)
+        return jsonify({"erro": f"Cultura '{nome_cultura}' não encontrada no banco de dados"}), 404
+    
+    # 3. O cálculo continua igual
     media = tabela_filtrada['producao_ton'].mean()
 
-    # 2. REGRA DE NEGÓCIO
     if media >= MEDIA_SAFRA_REFERENCIA:
         diagnostico = "Produtividade satisfatória. O solo mantém bons índices de rendimento."
     else:
         diagnostico = "Atenção! Produtividade abaixo do limiar aceitável. Recomenda-se iniciar a análise para rotação de culturas."
 
-    # 3. EMPACOTANDO A RESPOSTA NO FORMATO QUE ROBÔS LEEM (JSON/DICIONÁRIO)
     resposta = {
-        "cultura": "Milho",
+        "cultura": nome_cultura,
         "periodo_analisado_anos": len(tabela_filtrada),
         "media_toneladas": round(media, 2),
         "meta_referencia": MEDIA_SAFRA_REFERENCIA,
         "diagnostico": diagnostico
     }
 
-    # 4. DEVOLVENDO O PACOTE PARA QUEM PEDIU
     return jsonify(resposta)
 
-# Ligando o motor
 if __name__ == '__main__':
     app.run(debug=True)
